184dfbb000b5de25baaeb1f4d3dc98f0d1e601b4
Bode <289588@burlcoschools.org>
2026-03-27 09:14:49 -0400

    Update gurka.cpp

--- gurka.cpp
+++ gurka.cpp
@@ -2,38 +2,49 @@
 using namespace std;
 
 /* TO DO LIST
-1. Start screen
-2. Name input
-3. Gameplay
-4. Bot players
-5. Ascii art
+1. Start screen (COMPLETED)
+2. Name input 
+3. Gameplay 
+4. Bot players 
+5. Ascii art 
 */
 
-void titleCard(int& startScreenChoice);
+void titleCard(int& startScreenChoice); // Title screen function
+void instructions(int& instChoice);
 
 int main() {
     // Variables
-    int startScreenChoice;
+    int startScreenChoice, instChoice;
     
     
     titleCard(startScreenChoice);
-    if (startScreenChoice == )
+    if (startScreenChoice == 1) {
+        cout << "Game here eventually"; // game
+    } else if (startScreenChoice == 2) {
+        instructions(instChoice);
+    } else if (startScreenChoice == 3) {
+        cout << "Goodbye!";
+        return 0;
+    }
 }
 
+// title function
 void titleCard(int& startScreenChoice) {
-    cout << "==========================================\n";
-    cout << "|                                    ### |\n";
-    cout << "|  ####  #    # #####  #    #   ##   ### |\n";
-    cout << "| #    # #    # #    # #   #   #  #  ### |\n";
-    cout << "| #      #    # #    # ####   #    #  #  |\n";
-    cout << "| #  ### #    # #####  #  #   ######     |\n";
-    cout << "| #    # #    # #   #  #   #  #    # ### |\n";
-    cout << "|  ####   ####  #    # #    # #    # ### |\n";
-    cout << "==========================================\n\n";
+    cout << "======================\n";
+    cout << "   g  u  r  k  a  !   \n";
+    cout << "======================\n\n";
     cout << "1. Start the Game\n";
     cout << "2. How to Play\n";
     cout << "3. Quit\n\n";
-    cout << "==========================================\n\n";
+    cout << "======================\n\n";
     cout << "Choose a number: ";
-    cin >> startScreenChoice;
+    cin >> startScreenChoice;}
+
+// instruction function
+void instructions(int& instChoice) {
+    cout << "\n";
+    cout << "======================\n";
+    cout << "instructions\n";
+    cout << "======================\n";
+    cout << ""
 }
